19df14cafe03193d317032e0a6207e742282de50
John Glatts <johnglatts1@hotmail.com>
2022-12-28 11:22:51 -0500

    add simple demo code

--- Src/main.c
+++ Src/main.c
@@ -18,101 +18,48 @@
 #include "main.h"
 #include "cmsis_os.h"
 
-// add a mutex to hold button state
-// other threads can access
-// practice state machines with RTOS
+// Data to be shared by threads
+typedef struct Data {
+	int buffer[10];
+	int write_buff_idx;
+	int read_buff_idx;
+} Data;
+Data data;
 
+// Data mutex for threads
+SemaphoreHandle_t mutex;
+
+// Handles for threads
 osThreadId defaultTaskHandle;
 osThreadId buttonTask01Handle;
 
+
+// Function prototypes
 static void MX_GPIO_Init(void);
-void SystemClock_Config(void);
-void StartDefaultTask(void const * argument);
-void buttonTask(void const* argument);
+static void RTOS_Init(void);
+static int  writeData(void);
+static int  readData(void);
+void 		SystemClock_Config(void);
+void 		StartDefaultTask(void const * argument);
+void 		buttonTask(void const* argument);
 
 /**
   * @brief  The application entry point.
   * @retval int
   */
 int main(void) {
+	// Init device peripherals
+	HAL_Init();
+	SystemClock_Config();
+	MX_GPIO_Init();
 
-  HAL_Init();
-  SystemClock_Config();
-  MX_GPIO_Init();
+	// Init RTOS and start the scheduler
+	RTOS_Init();
+	osKernelStart();
 
-  osThreadDef(defaultTask, StartDefaultTask, osPriorityNormal, 0, 128);
-  defaultTaskHandle = osThreadCreate(osThread(defaultTask), NULL);
-
-  osThreadDef(buttonTask01, buttonTask, osPriorityNormal, 0, 128);
-  buttonTask01Handle = osThreadCreate(osThread(buttonTask01), NULL);
-
-  osKernelStart();
-
-  while (1) ; // shouldn't get here
-}
-
-/**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void) {
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
-    Error_Handler();
-  }
-
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
-
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK) {
-    Error_Handler();
-  }
-}
-
-/**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_GPIO_Init(void) {
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
-
-  /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOC_CLK_ENABLE();
-  __HAL_RCC_GPIOD_CLK_ENABLE();
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET);
-
-  /*Configure GPIO pin : LED_Pin */
-  GPIO_InitStruct.Pin = LED_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(LED_GPIO_Port, &GPIO_InitStruct);
-
-  /*Configure GPIO pin : BTN_Pin */
-  GPIO_InitStruct.Pin = BTN_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_PULLUP;
-  HAL_GPIO_Init(BTN_GPIO_Port, &GPIO_InitStruct);
-
+	// Shouldn't get here
+	// RTOS scheduler will take over
+	while (1);
 }
 
 /**
@@ -121,10 +68,13 @@ static void MX_GPIO_Init(void) {
   * @retval None
   */
 void StartDefaultTask(void const* argument) {
-  while(1) {
-	// get button state and then change led
-    osDelay(1);
-  }
+	while(1) {
+		xSemaphoreTake(mutex, portMAX_DELAY);
+		int state = readData();
+		xSemaphoreGive(mutex);
+		// add some more peripherals and do stuff based on state
+		osDelay(1);
+	}
 }
 
 /**
@@ -133,14 +83,119 @@ void StartDefaultTask(void const* argument) {
 * @retval None
 */
 void buttonTask(void const* argument) {
-  // main loop for button reading task
-  while (1) {
-	  if (HAL_GPIO_ReadPin(BTN_GPIO_Port, BTN_Pin) != GPIO_PIN_SET) {
-		  HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_SET);
-		  osDelay(100);
-		  HAL_GPIO_WritePin(BTN_GPIO_Port, BTN_Pin, GPIO_PIN_RESET);
-	  }
-  }
+	// main loop for button reading task
+	while (1) {
+		xSemaphoreTake(mutex, portMAX_DELAY);
+		int state = writeData();
+		xSemaphoreGive(mutex);
+		if (state != GPIO_PIN_SET) {
+			HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_SET);
+			osDelay(100);
+			HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET);
+		}
+    }
+}
+
+static int readData(void) {
+	int state = data.buffer[data.read_buff_idx++];
+	if (data.read_buff_idx == 10) {
+		data.read_buff_idx = 0;
+	}
+	return state;
+}
+
+/**
+* @brief Function to write to the data buffer
+* @retval None
+*/
+static int writeData(void) {
+	int state = HAL_GPIO_ReadPin(BTN_GPIO_Port, BTN_Pin);
+	data.buffer[data.write_buff_idx++] = state;
+	if (data.write_buff_idx == 10) {
+		data.write_buff_idx = 0;
+	}
+	// debug
+	if ((data.write_buff_idx % 2) == 0) {
+		data.buffer[data.write_buff_idx] = 0;
+		state = 0;
+	}
+	return state;
+}
+
+/**
+  * @brief Initialize variables for RTOS tasks
+  * @retval None
+  */
+static void RTOS_Init(void) {
+	mutex = xSemaphoreCreateMutex();
+
+	osThreadDef(defaultTask, StartDefaultTask, osPriorityNormal, 0, 128);
+	defaultTaskHandle = osThreadCreate(osThread(defaultTask), NULL);
+
+	osThreadDef(buttonTask01, buttonTask, osPriorityNormal, 0, 128);
+	buttonTask01Handle = osThreadCreate(osThread(buttonTask01), NULL);
+
+	data.write_buff_idx = 0;
+	data.read_buff_idx = 0;
+	for (int i = 0; i < 10; ++i) {
+		data.buffer[i] = 0;
+	}
+
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void) {
+	RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+	RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+	Error_Handler();
+	}
+
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+							  |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK) {
+	Error_Handler();
+	}
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void) {
+	GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+	__HAL_RCC_GPIOC_CLK_ENABLE();
+	__HAL_RCC_GPIOD_CLK_ENABLE();
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+
+	HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET);
+
+	GPIO_InitStruct.Pin = LED_Pin;
+	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+	HAL_GPIO_Init(LED_GPIO_Port, &GPIO_InitStruct);
+
+	GPIO_InitStruct.Pin = BTN_Pin;
+	GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+	GPIO_InitStruct.Pull = GPIO_PULLUP;
+	HAL_GPIO_Init(BTN_GPIO_Port, &GPIO_InitStruct);
+
 }
 
 /**
@@ -148,7 +203,6 @@ void buttonTask(void const* argument) {
   * @retval None
   */
 void Error_Handler(void) {
-  __disable_irq();
-  while (1)  {
-  }
+	__disable_irq();
+	while (1);
 }
